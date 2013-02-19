ed2ff2d799192d14a4cea67e9205038fe3894693
Ryan Detzel <ryan.detzel@gmail.com>
2013-02-19 20:21:11 +0000

    initial commit

---
+++ blimp_commit.py
@@ -0,0 +1,55 @@
+import json
+import re
+
+import blimp
+from flask import Flask, request
+
+app = Flask(__name__)
+
+api_username = ""
+api_key = ""
+app_id = ""
+app_secret = ""
+blimp_userid = ""
+
+
+def update_task(task_id, commit_msg, url, author):
+    api = blimp.Client(api_username, api_key, app_id, app_secret)
+    comment = "%s: %s\n%s" % (
+        author, 
+        commit_msg.replace('t%s' % task_id, '').replace('#done', ''),
+        url)
+    comment_obj = {
+        'comment': comment,
+        'object_pk': task_id,
+        'user': u'/api/v2/user/%s/' % blimp_userid,
+        'content_type': u'todo',
+    }
+    comment_req = api.comment.create(comment_obj)
+    if '#done' in commit_msg:
+        task = api.task.get(task_id)
+        task_req = api.task.update(task['id'], {"state": "done"})
+
+
+@app.route("/hook/github", methods=['POST'])
+def gh_hook():
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        if 'commits' in data:
+            for commit in data['commits']:
+                message = commit['message']
+                match = re.search(r't(\d+)\s*', message)
+                if match:
+                    task_id = match.group(1)
+                    update_task(
+                        task_id,
+                        message,
+                        commit['url'],
+                        commit['author']['name']
+                    )
+
+    return "Okay"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=8002)
